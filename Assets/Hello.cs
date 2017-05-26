0c8f44d3f85f3b0ad2e14bc5d1a22b7d3ef68fcf
Tak <levi@unity3d.com>
2017-05-26 11:08:50 +0200

    Initial commit

---
+++ Assets/Hello.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+	Rect _labelRect;
+	static readonly string _labelText = "Hello Bugzilla!";
+
+	void Start ()
+	{
+		_labelRect = new Rect (Screen.width / 2, Screen.height / 2, Screen.width, Screen.height);
+	}
+
+	void OnGUI ()
+	{
+		GUI.Label (_labelRect, _labelText);
+	}
+}
